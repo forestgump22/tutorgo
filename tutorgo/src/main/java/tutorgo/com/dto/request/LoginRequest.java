fc03b094e7ec2440324b4b07c8880d7c8a1aed95
Bruno Agnini <elliabella41@gmail.com>
2025-06-24 09:00:00 -0500

    Implementacion de la HU10

--- tutorgo/src/main/java/tutorgo/com/dto/request/LoginRequest.java
+++ tutorgo/src/main/java/tutorgo/com/dto/request/LoginRequest.java
@@ -6,11 +6,11 @@ import lombok.Data;
 
 @Data
 public class LoginRequest {
- 
+    
     @NotBlank(message = "El correo electrónico es obligatorio")
     @Email(message = "El formato del correo electrónico no es válido")
     private String email;
- 
+  
     @NotBlank(message = "La contraseña es obligatoria")
-    private String password;
+    private String password; 
 }
